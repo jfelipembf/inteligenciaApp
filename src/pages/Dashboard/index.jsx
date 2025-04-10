9310123a17811276055e543f24a6fa37e1105c25
Ytallo <ytallo4gamer@gmail.com>
2025-04-10 16:58:09 -0300

    fetch que pega quantidade dos usuarios para dashboard

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -1,13 +1,8 @@
 import PropTypes from "prop-types";
 import React from "react";
-import {
-  Container,
-  Row,
-  Col,
-  Button,
-  Card,
-  CardBody,
-} from "reactstrap";
+import { Container, Row, Col, Button, Card, CardBody } from "reactstrap";
+import useFetchStudents from "../../hooks/useFetchStudents";
+import useFetchUsers from "../../hooks/useFetchUsers";
 
 //Import Breadcrumb
 import Breadcrumbs from "../../components/Common/Breadcrumb";
@@ -22,12 +17,22 @@ import { withTranslation } from "react-i18next";
 const Dashboard = (props) => {
   // Definindo o perfil como gestor permanentemente
   const userRole = "gestor";
+  const { students, loading, error } = useFetchStudents(userRole);
+  const { users } = useFetchUsers();
 
   // Dados para o perfil de Gestor/Coordenador
   const reports = [
-    { title: "Número de Alunos", iconClass: "bx-user", description: "1,235" },
-    { title: "Usuários Ativos", iconClass: "bx-check-circle", description: "980" },
-    { title: "Usuários Inativos", iconClass: "bx-x-circle", description: "255" },
+    {
+      title: "Número de Alunos",
+      iconClass: "bx-user",
+      description: students.length,
+    },
+    {
+      title: "Usuários Ativos",
+      iconClass: "bx-check-circle",
+      description: users.length,
+    },
+    // { title: "Usuários Inativos", iconClass: "bx-x-circle", description: "255" },
   ];
 
   //meta title
@@ -38,26 +43,29 @@ const Dashboard = (props) => {
       <div className="page-content">
         <Container fluid>
           {/* Render Breadcrumb */}
-          <Breadcrumbs title={props.t("Dashboards")} breadcrumbItem={props.t("Dashboard")} />
+          <Breadcrumbs
+            title={props.t("Dashboards")}
+            breadcrumbItem={props.t("Dashboard")}
+          />
 
-          
-          
           {/* Cards */}
           <Row>
             {(reports || [])?.map((report, key) => (
-              <Col md="4" key={"_col_" + key}>
+              <Col md="6" key={"_col_" + key}>
                 <Card className="mini-stats-wid">
                   <CardBody>
                     <div className="d-flex">
                       <div className="flex-grow-1">
-                        <p className="text-muted fw-medium">
-                          {report.title}
-                        </p>
+                        <p className="text-muted fw-medium">{report.title}</p>
                         <h4 className="mb-0">{report.description}</h4>
                       </div>
                       <div className="avatar-sm rounded-circle bg-primary align-self-center mini-stat-icon">
                         <span className="avatar-title rounded-circle bg-primary">
-                          <i className={"bx " + report.iconClass + " font-size-24"}></i>
+                          <i
+                            className={
+                              "bx " + report.iconClass + " font-size-24"
+                            }
+                          ></i>
                         </span>
                       </div>
                     </div>
@@ -66,16 +74,13 @@ const Dashboard = (props) => {
               </Col>
             ))}
           </Row>
-          
-          
+
           {/* Lista de Médias por Turma */}
           <Row className="mt-4">
             <Col lg="12">
               <Card>
                 <CardBody>
-                  <h4 className="card-title mb-4">
-                    Média Geral por Turma
-                  </h4>
+                  <h4 className="card-title mb-4">Média Geral por Turma</h4>
                   <ClassAveragesList userRole={userRole} />
                 </CardBody>
               </Card>
