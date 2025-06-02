69ab01385106cede067a5505e910c4348b87fa12
Ytallo <ytallo4gamer@gmail.com>
2025-06-02 14:24:58 -0300

    Criação base para alerts

--- src/hooks/useCreateNotification.js
+++ src/hooks/useCreateNotification.js
@@ -26,20 +26,82 @@ const useCreateNotification = () => {
       // Adiciona remetente e data de criação
       const notification = {
         ...notificationData,
-        sentBy: {label: userDetails.personalInfo?.name,
-          value: userDetails.uid
+        sentBy: {
+          label: userDetails.personalInfo?.name,
+          value: userDetails.uid,
         },
         createdAt: brTime,
         status: "created",
       };
 
-      console.log("schoolId", schoolId);
-      await firebase
+      const notificationRef = await firebase
         .firestore()
         .collection("schools")
         .doc(schoolId)
         .collection("notifications")
         .add(notification);
+
+      const maxDescriptionLength = 60;
+
+      let recipients = [];
+      let description = "";
+      if (
+        notificationData.type === "individual" &&
+        notificationData.individual?.value
+      ) {
+        recipients = [notificationData.individual.value];
+        description = `Você recebeu uma notificação de ${userDetails.personalInfo?.name}`;
+      } else if (
+        notificationData.type === "class" &&
+        notificationData.class?.value
+      ) {
+        const studentsSnapshot = await firebase
+          .firestore()
+          .collection("schools")
+          .doc(schoolId)
+          .collection("classes")
+          .doc(notificationData.class.value)
+          .collection("students")
+          .get();
+
+        recipients = studentsSnapshot.docs.map((doc) => doc.id);
+
+        if (notificationData.lesson?.label) {
+          description = `${userDetails.personalInfo?.name} enviou uma notificação relativa a matéria de ${notificationData.lesson.label}`;
+        } else {
+          description = `${userDetails.personalInfo?.name} enviou uma notificação para a turma ${notificationData.class.label}`;
+        }
+      } else if (notificationData.type === "school") {
+        recipients = userDetails.schoolId;
+        description = `${userDetails.personalInfo?.name} enviou uma notificação para toda a escola`;
+      }
+
+      const alert = {
+        title: notificationData.title,
+        message: notificationData.description
+          ? notificationData.description.slice(0, maxDescriptionLength) +
+            (notificationData.description.length > maxDescriptionLength
+              ? "..."
+              : "")
+          : "",
+        description: description,
+        type: "notification",
+        sentBy: {
+          label: userDetails.personalInfo?.name,
+          value: userDetails.uid,
+        },
+        referenceId: notificationRef.id,
+        createdAt: brTime,
+        recipients: recipients || [],
+        schedule: notificationData.schedule || null,
+      };
+
+      await firebase
+        .firestore()
+        .collection("schools")
+        .doc(schoolId)
+        .collection("alerts")
+        .add(alert);
 
       setLoading(false);
       return { success: true };
